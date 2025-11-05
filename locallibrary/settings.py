d86156236c44c57471b2b966e3a062b14eef1640
kbulbul026-coder <kbulbul026@gmail.com>
2025-11-05 09:23:35 +0530

    add database url

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -13,7 +13,7 @@ import os
 from dotenv import load_dotenv
 
 from pathlib import Path
-import dj_database_url
+#import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
